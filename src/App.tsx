3749559808ee1956972f921d778f15101258b1f2
Nikolai_Iakushchenko <nick22783@gmail.com>
2023-08-28 17:13:51 +0300

    Setting Up Categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 
+const categories = [
+  { id: 1, title: "Hats" },
+  { id: 2, title: "Jackets" },
+  { id: 3, title: "Sneakers" },
+  { id: 4, title: "Womens" },
+  { id: 5, title: "Mens" },
+];
+
 const App = () => {
-  return <div>Hi</div>;
+  return (
+    <div className="categories-container">
+      {categories.map(({ title, id }) => (
+        <div className="category-container">
+          <div className="background-image" />
+          <div key={id} className="category-body-conainer">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
