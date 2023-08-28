d62d07b78bc105802afd56268bb8a3b10699af6e
Nikolai_Iakushchenko <nick22783@gmail.com>
2023-08-28 18:30:07 +0300

    add category-item.tsx

---
+++ src/components/category-item/category-item.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import "./category-item.styles.scss";
+
+interface CategoryItemProps {
+  category: {
+    id: number;
+    imageUrl: string;
+    title: string;
+  };
+}
+
+const CategoryItem = ({ category }: CategoryItemProps) => {
+  const { imageUrl, title } = category;
+
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
