d226ab628f170f6537e05e4376ebea3bcb8614d8
Nikolai_Iakushchenko <nick22783@gmail.com>
2023-08-29 11:52:09 +0300

    add home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,6 @@
 import React from "react";
-import Directory from "./components/directory/directory";
 import { Link, Route, Routes } from "react-router-dom";
-
-const categories = [
-  {
-    id: 1,
-    title: "hats",
-    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-  },
-  {
-    id: 2,
-    title: "jackets",
-    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-  },
-  {
-    id: 3,
-    title: "sneakers",
-    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-  },
-  {
-    id: 4,
-    title: "womens",
-    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-  },
-  {
-    id: 5,
-    title: "mens",
-    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-  },
-];
+import Home from "./routes/home/home";
 
 enum ROUTES {
   HOME = "/",
@@ -44,10 +16,7 @@ const App = () => {
         <Link to={ROUTES.ANOTHER_PAGE}>Another page</Link>
       </header>
       <Routes>
-        <Route
-          path={ROUTES.HOME}
-          element={<Directory categories={categories} />}
-        />
+        <Route path={ROUTES.HOME} element={<Home />} />
         <Route path={ROUTES.ANOTHER_PAGE} element={"another page"} />
       </Routes>
     </>
