ff67d6ac695ec1bef5dadcacbc4076504d751cc6
Nick <nick22783@gmail.com>
2023-08-30 15:20:29 +0300

    wip add handleSubmit

--- src/components/sign-up-form/sign-up-form.tsx
+++ src/components/sign-up-form/sign-up-form.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { createAuthUserWithEmailAndPassword } from "../../utils/firebase/firebase.utils";
 
 const defaultFormFields = {
   displayName: "",
@@ -12,11 +13,50 @@ const SignUpForm = () => {
 
   console.log("formFields", formFields);
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = async (event: React.SyntheticEvent) => {
     event.preventDefault();
+
+    const target = event.target as typeof event.target & {
+      password: { value: string };
+      confirmPassword: { value: string };
+    };
+
     //   todo confirm that passwords match
+    const password = target.password.value;
+    const confirmedPassword = target.confirmPassword.value;
+
+    if (password !== confirmedPassword) {
+      return console.error("Passwords don't match");
+      // throw new Error("Passwords don't match");
+    }
+
+    console.log("event", event);
+    console.log("password", password);
     // todo see if we authenticated user with email and password
-    //todo create a user document from what createAuthUserWithEmailAndPassword returns
+    try {
+      const response = await createAuthUserWithEmailAndPassword(
+        email,
+        password,
+      );
+      console.log("response", response);
+      if (!response) {
+        return console.error("No response from the server!!");
+      }
+
+      interface User {
+        email: string;
+      }
+
+      //todo create a user document from what createAuthUserWithEmailAndPassword returns
+      const user = response.user as User;
+      const userDocument = {
+        displayName: user.email.substring(0, user.email.indexOf("@")),
+        email: user.email,
+      };
+      console.log("userDocument", userDocument);
+    } catch (error: any) {
+      console.error("Sign in error:", error.message);
+    }
   };
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -27,7 +67,7 @@ const SignUpForm = () => {
   return (
     <div>
       <h1>Sign up with your email and password</h1>
-      <form onSubmit={() => {}}>
+      <form onSubmit={handleSubmit}>
         <label>Display Name</label>
         <input
           type="text"
