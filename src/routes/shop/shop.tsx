dbf96fa41d7038a9b0e30411616248f6b6d78a94
Nick <nick22783@gmail.com>
2023-09-06 17:56:10 +0300

    update shop to styled component

--- src/routes/shop/shop.tsx
+++ src/routes/shop/shop.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import CategoriesPreview from "../categories-preview/categories-preview";
-import "./shop.scss";
 import Category from "../category/category";
 
 const Shop = () => {
