3e5abe03b061b9a2f33a3d3ab294f19ee6e9ef91
Nick <nick22783@gmail.com>
2023-09-09 12:09:06 +0300

    add reselect library

--- src/store/categories/category.selector.ts
+++ src/store/categories/category.selector.ts
@@ -1,15 +1,24 @@
 import { RootState } from "../store";
 import { CategoriesMap, ICategory } from "./category.types";
+import { createSelector } from "reselect";
 
-export const selectCategoriesMap = (state: RootState) => {
-  console.log("selector fired");
+const selectCatagroyReducer = (state: RootState) =>
+  state.categories;
 
-  return state.categories.categories.reduce(
-    (acc: CategoriesMap, category: ICategory) => {
-      const { title, items } = category;
-      acc[title.toLowerCase()] = items;
-      return acc;
-    },
-    {},
-  );
-};
+export const selectCategories = createSelector(
+  [selectCatagroyReducer],
+  (categoriesSlice) => categoriesSlice.categories,
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) =>
+    categories.reduce(
+      (acc: CategoriesMap, category: ICategory) => {
+        const { title, items } = category;
+        acc[title.toLowerCase()] = items;
+        return acc;
+      },
+      {},
+    ),
+);
